a2f89815c9402632f5cba352ce6ffde0d16d9dd6
Johan Nordström <johan@digitalvillage.se>
2024-02-08 00:05:02 +0100

    Initial commit.

---
+++ src/routes/index.ts
@@ -0,0 +1,32 @@
+/**
+ * Main application routes
+ */
+import express from "express";
+import resourceRouter from "./_router";
+const router = express.Router();
+
+/**
+ * GET /
+ */
+router.get("/", (req, res) => {
+	res.send({
+		message: "But first, let me take a selfie 🤳 https://www.youtube.com/watch?v=kdemFfbS5H0",
+	});
+});
+
+/**
+ * [EXAMPLE] /resource
+ */
+// router.use('/resource', resourceRouter);
+
+/**
+ * Catch-all route handler
+ */
+router.use((req, res) => {
+	// Respond with 404 and a message in JSON-format
+	res.status(404).send({
+		message: "Not Found",
+	});
+});
+
+export default router;
